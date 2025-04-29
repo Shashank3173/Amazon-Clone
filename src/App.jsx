812339ee1053d53e350868d9f1420520f8140c07
Shashank3173 <163388187+Shashank3173@users.noreply.github.com>
2025-04-29 14:39:39 +0530

    Fix routing with HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import Footer from "./Compoenents/Footer";
 import Header from "./Compoenents/Header";
-import { Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import CartPage from "./Pages/CartPage";
 import ProductPaga from "./Pages/ProductPaga";
@@ -9,20 +9,18 @@ import Context from "./ContextApi/Context";
 
 function App() {
   return (
-    <>
     <Context>
-      <Header />
+      <Router>
+        <Header />
         <Routes>
-          <Route path="/" index element={<Home />}/>
-          <Route path="/CartPage" element={<CartPage />}/>
-          <Route path={`/ProductPaga/:id`} element={<ProductPaga />}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/CartPage" element={<CartPage />} />
+          <Route path="/ProductPaga/:id" element={<ProductPaga />} />
         </Routes>
-      <Footer />
-      </Context>
-    </>
+        <Footer />
+      </Router>
+    </Context>
   );
 }
 
 export default App;
-
-CartPage
